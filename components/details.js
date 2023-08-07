28b21e0a47b06eaac7c7fd0025faed86384b5f49
leoc01 <leonardocampetti@gmail.com>
2023-08-07 16:12:27 +0100

    Animação do ícone na tag `details`

--- components/details.js
+++ components/details.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 function Details({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = (e) => {
+    setIsOpen(e.target.open);
+  };
+
   return (
     <>
-      <details className="px-2 max-w-3xl cursor-pointer">
+      <details
+        className="px-2 max-w-3xl cursor-pointer"
+        open={isOpen}
+        onToggle={handleToggle}
+      >
         <summary className="flex items-center gap-10 justify-between">
           <p className="text-lg py-2">{title}</p>
-          <div className="text-4xl text-green-600">+</div>
+          <div
+            className={`transform transition-transform duration-200 text-4xl text-green-600 ${
+              isOpen ? "rotate-45 text-red-600" : ""
+            }`}
+          >
+            +
+          </div>
         </summary>
         <div className="pb-4 pl-2 text-lg text-gray-800 max-w-2xl cursor-default">
           {children}
@@ -19,6 +37,10 @@ function Details({ title, children }) {
 
           summary::-webkit-details-marker {
             display: none; /* Chrome */
+          }
+
+          .rotate-45 {
+            transform: rotate(45deg);
           }
         `}
       </style>
